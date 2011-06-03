0aff608c8f918eece8e947b5cc5fa450c50a608d
lintool <jimmylin@umd.edu>
2011-06-03 00:34:47 -0400

    Cleaned up CascadeBM25ScoringFunction and CascadeDirichletScoringFunction.

--- src/java/main/ivory/smrf/model/score/CascadeDirichletScoringFunction.java
+++ src/java/main/ivory/smrf/model/score/CascadeDirichletScoringFunction.java
@@ -1,11 +1,11 @@
 /*
  * Ivory: A Hadoop toolkit for web-scale information retrieval
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you
  * may not use this file except in compliance with the License. You may
  * obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0 
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,9 +16,6 @@
 
 package ivory.smrf.model.score;
 
-import ivory.smrf.model.GlobalEvidence;
-import ivory.smrf.model.GlobalTermEvidence;
-import ivory.util.XMLTools;
 import ivory.util.RetrievalEnvironment;
 
 import org.w3c.dom.Node;
@@ -28,38 +25,25 @@ import org.w3c.dom.Node;
  * 
  */
 public class CascadeDirichletScoringFunction extends DirichletScoringFunction {
+  private static float staticMU;
 
-        private static float MU;
+  @Override
+  public void configure(Node domNode) {
+    super.configure(domNode);
+    staticMU = mu;
+  }
 
-	
-	@Override
-	public synchronized void configure(Node domNode) {
-		super.configure(domNode);
-		MU = mu;
-	}
+  @Override
+  public float getScore(int tf, int docLen) {
+    if (isOOV) {
+      return 0.0f;
+    }
 
+    // Since cascade is trained this way (term and term proximity features have same mu)
+    if (RetrievalEnvironment.mIsNewModel) {
+      return (float) Math.log(((float) tf + staticMU * backgroundProb) / (docLen + staticMU));
+    }
 
-        @Override
-        public float getScore(int tf, int docLen) {
-//          if ( MU != mu ) {
-//            throw new RuntimeException("MU=" + MU + ", mu=" + mu + ", " + RetrievalEnvironment.mIsNewModel);
-//          }
-                if (isOOV) {
-                        return 0.0f;
-                }
-
-                //Since cascade is trained this way (term and term proximity features have same mu)
-                if (RetrievalEnvironment.mIsNewModel){
-                        return (float) Math.log(((float) tf + MU * backgroundProb) / (docLen + MU));
-                }
-                else{
-                        return (float) Math.log(((float) tf + mu * backgroundProb) / (docLen + mu));
-                }
-        }
-
-//	@Override
-//	public void initialize(GlobalTermEvidence termEvidence, GlobalEvidence globalEvidence) {
-//		super.initialize(termEvidence, globalEvidence);
-//		collectionLength = (float) globalEvidence.collectionLength;
-//	}
+    return super.getScore(tf, docLen);
+  }
 }
