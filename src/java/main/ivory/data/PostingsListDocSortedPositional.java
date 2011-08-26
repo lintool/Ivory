40da4477f2b6e4940cd60c605bef855fc62eac12
lintool <jimmylin@umd.edu>
2011-08-26 17:22:44 -0400

    Fixed bug caused by refactoring of LazyIntDocVector.

--- src/java/main/ivory/data/PostingsListDocSortedPositional.java
+++ src/java/main/ivory/data/PostingsListDocSortedPositional.java
@@ -1,4 +1,13 @@
 package ivory.data;
 
+import org.apache.log4j.Logger;
+
+@Deprecated
 public class PostingsListDocSortedPositional
-    extends ivory.core.data.index.PostingsListDocSortedPositional {}
+    extends ivory.core.data.index.PostingsListDocSortedPositional {
+  private static final Logger LOG = Logger.getLogger(PostingsListDocSortedPositional.class);
+  static {
+    LOG.warn("You are using a old version of the index! " +
+        PostingsListDocSortedPositional.class.getCanonicalName() + " is depcrecated.");
+  }
+}
