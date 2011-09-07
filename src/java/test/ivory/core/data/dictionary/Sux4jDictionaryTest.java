3131cb9516b401029009dc9bb64cc7321d140302
lintool <jimmylin@umd.edu>
2011-09-06 23:04:02 -0400

    Experimented with FrontCodedStringList from DSI utils.

--- src/java/test/ivory/core/data/dictionary/Sux4jDictionaryTest.java
+++ src/java/test/ivory/core/data/dictionary/Sux4jDictionaryTest.java
@@ -3,6 +3,7 @@ package ivory.core.data.dictionary;
 import it.unimi.dsi.bits.TransformationStrategies;
 import it.unimi.dsi.sux4j.mph.MinimalPerfectHashFunction;
 import it.unimi.dsi.sux4j.mph.TwoStepsLcpMonotoneMinimalPerfectHashFunction;
+import it.unimi.dsi.util.FrontCodedStringList;
 import it.unimi.dsi.util.ShiftAddXorSignedStringMap;
 
 import java.io.IOException;
@@ -75,7 +76,7 @@ public class Sux4jDictionaryTest extends TestCase {
       dictionary.getId(t);
     }
     System.out.println("Total time: " + (System.currentTimeMillis() - start) + "ms");
-
+    
     ShiftAddXorSignedStringMap dict = new ShiftAddXorSignedStringMap(dictionary.iterator(),
           new TwoStepsLcpMonotoneMinimalPerfectHashFunction<CharSequence>(dictionary,
               TransformationStrategies.prefixFreeIso()));
@@ -88,6 +89,48 @@ public class Sux4jDictionaryTest extends TestCase {
     for (String t : randomTerms) {
       dict.getLong(t);
     }
-    System.out.println("Total time: " + (System.currentTimeMillis() - start) + "ms");
+    System.out.println("Total time (lookup): " + (System.currentTimeMillis() - start) + "ms");
+  }
+
+  // Looking up terms from termids, comparing DSI utils with Ivory.
+  @Test
+  public void test3() throws IOException {
+    Configuration conf = new Configuration();
+    FileSystem fs = FileSystem.get(conf);
+
+    long bytes = Runtime.getRuntime().freeMemory();
+    Dictionary dictionary =
+        PrefixEncodedLexicographicallySortedDictionary.load(
+            new Path("etc/trec-index-terms.dat"), fs);
+    System.out.println("Ivory: Memory usage: " + (bytes - Runtime.getRuntime().freeMemory()));
+
+    bytes = Runtime.getRuntime().freeMemory();
+    FrontCodedStringList dsiStringList = new FrontCodedStringList(dictionary.iterator(), 8, false);
+    System.out.println("DSI: Memory usage:   " + (bytes - Runtime.getRuntime().freeMemory()));
+
+    Random rand = new Random();
+    List<Integer> randomTermIds = Lists.newArrayList();
+    for (int i = 0; i < 1000000; i++) {
+      int r = rand.nextInt(dictionary.size());
+      randomTermIds.add(r);
+    }
+
+    long start;
+
+    start = System.currentTimeMillis();
+    for (Integer i : randomTermIds) {
+      assertFalse(dictionary.getTerm(i) == null);
+    }
+    System.out.println("Ivory: Total time: " + (System.currentTimeMillis() - start) + "ms");
+
+    start = System.currentTimeMillis();
+    for (Integer i : randomTermIds) {
+      assertFalse(dsiStringList.get(i) == null);
+    }
+    System.out.println("DSI: Total time:   " + (System.currentTimeMillis() - start) + "ms");
+
+    for (Integer i : randomTermIds) {
+      assertTrue(dictionary.getTerm(i).equals(dsiStringList.get(i).toString()));
+    }
   }
 }
