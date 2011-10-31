b5df90481ad5854c8138b5f1108523eb22da6d31
Earl J. Wagner <ewagner@umiacs.umd.edu>
2011-10-30 21:39:59 -0400

    minor changes

--- src/java/main/ivory/core/driver/PreprocessAquaint2.java
+++ src/java/main/ivory/core/driver/PreprocessAquaint2.java
@@ -74,8 +74,9 @@ public class PreprocessAquaint2 extends Configured implements Tool {
     // Create the index directory if it doesn't already exist.
     Path p = new Path(indexRootPath);
     if (!fs.exists(p)) {
-      LOG.info("index directory doesn't exist, creating...");
       fs.mkdirs(p);
+    } else {
+      LOG.info("Index directory already exists, skipping!");
     }
 
     RetrievalEnvironment env = new RetrievalEnvironment(indexRootPath, fs);
@@ -86,53 +87,53 @@ public class PreprocessAquaint2 extends Configured implements Tool {
     Path mappingDir = env.getDocnoMappingDirectory();
 
     if (!fs.exists(mappingFile)) {
-      LOG.info("docno-mapping.dat doesn't exist, creating...");
       String[] arr = new String[] { collection, mappingDir.toString(),
-              mappingFile.toString() };
+                                    mappingFile.toString() };
       NumberAquaint2Documents2 tool = new NumberAquaint2Documents2();
       tool.setConf(conf);
       tool.run(arr);
       fs.delete(mappingDir, true);
+    } else {
+      LOG.info("DocnoMapping already exists, skipping!");
+    }
+    Aquaint2DocnoMapping dm = new Aquaint2DocnoMapping();
+    dm.loadMapping(mappingFile, fs);
 
-	  Aquaint2DocnoMapping dm = new Aquaint2DocnoMapping();
-	  dm.loadMapping(mappingFile, fs);
 
-	  int docno; int expectedDocno; String expectedDocid; String docid;
-	  boolean testAquaint2 = false;
-	  if (testAquaint2) {
-		  docno = 500; expectedDocid = "AFP_ENG_20041001.0500"; docid = dm.getDocid(docno);
-		  System.out.println ("dm.getDocid(" + docno + "): " + docid + ", should be: " + expectedDocid + ", " + (expectedDocid.equals(docid)));
-		  docno = 600; expectedDocid = "AFP_ENG_20041001.0600"; docid = dm.getDocid(docno);
-		  System.out.println ("dm.getDocid(" + docno + "): " + docid + ", should be: " + expectedDocid + ", " + (expectedDocid.equals(docid)));
-		  docno = 700; expectedDocid = "AFP_ENG_20041001.0701"; docid = dm.getDocid(docno);
-		  System.out.println ("dm.getDocid(" + docno + "): " + docid + ", should be: " + expectedDocid + ", " + (expectedDocid.equals(docid)));
-		  docno = 800; expectedDocid = "AFP_ENG_20041003.0019"; docid = dm.getDocid(docno);
-		  System.out.println ("dm.getDocid(" + docno + "): " + docid + ", should be: " + expectedDocid + ", " + (expectedDocid.equals(docid)));
-		  expectedDocno = 500; docid = "AFP_ENG_20041001.0500"; docno = dm.getDocno(docid);
-		  System.out.println ("dm.getDocno(" + docid + "): " + docno + ", should be: " + expectedDocno + ", " + (expectedDocno == docno));
-		  expectedDocno = 600; docid = "AFP_ENG_20041001.0600"; docno = dm.getDocno(docid);
-		  System.out.println ("dm.getDocno(" + docid + "): " + docno + ", should be: " + expectedDocno + ", " + (expectedDocno == docno));
-		  expectedDocno = 700; docid = "AFP_ENG_20041001.0701"; docno = dm.getDocno(docid);
-		  System.out.println ("dm.getDocno(" + docid + "): " + docno + ", should be: " + expectedDocno + ", " + (expectedDocno == docno));
-		  expectedDocno = 800; docid = "AFP_ENG_20041003.0019"; docno = dm.getDocno(docid);
-		  System.out.println ("dm.getDocno(" + docid + "): " + docno + ", should be: " + expectedDocno + ", " + (expectedDocno == docno));
-		  return 0;
-	  }
-	  boolean testGigaword = false;
-	  if (testGigaword) {
-		  for (int i = 1; i < 301; i++) {
-			  docno = i * 1000;
-			  docid = dm.getDocid(docno);
-			  System.out.println ("dm.getDocid(" + docno + "): " + docid);
-		  }
-		  return 0;
-	  }
+    int docno; int expectedDocno; String expectedDocid; String docid;
+    boolean testAquaint2 = false;
+    if (testAquaint2) {
+      docno = 500; expectedDocid = "AFP_ENG_20041001.0500"; docid = dm.getDocid(docno);
+      System.out.println("dm.getDocid(" + docno + "): " + docid + ", should be: " + expectedDocid + ", " + (expectedDocid.equals(docid)));
+      docno = 600; expectedDocid = "AFP_ENG_20041001.0600"; docid = dm.getDocid(docno);
+      System.out.println("dm.getDocid(" + docno + "): " + docid + ", should be: " + expectedDocid + ", " + (expectedDocid.equals(docid)));
+      docno = 700; expectedDocid = "AFP_ENG_20041001.0701"; docid = dm.getDocid(docno);
+      System.out.println("dm.getDocid(" + docno + "): " + docid + ", should be: " + expectedDocid + ", " + (expectedDocid.equals(docid)));
+      docno = 800; expectedDocid = "AFP_ENG_20041003.0019"; docid = dm.getDocid(docno);
+      System.out.println("dm.getDocid(" + docno + "): " + docid + ", should be: " + expectedDocid + ", " + (expectedDocid.equals(docid)));
+      expectedDocno = 500; docid = "AFP_ENG_20041001.0500"; docno = dm.getDocno(docid);
+      System.out.println("dm.getDocno(" + docid + "): " + docno + ", should be: " + expectedDocno + ", " + (expectedDocno == docno));
+      expectedDocno = 600; docid = "AFP_ENG_20041001.0600"; docno = dm.getDocno(docid);
+      System.out.println("dm.getDocno(" + docid + "): " + docno + ", should be: " + expectedDocno + ", " + (expectedDocno == docno));
+      expectedDocno = 700; docid = "AFP_ENG_20041001.0701"; docno = dm.getDocno(docid);
+      System.out.println("dm.getDocno(" + docid + "): " + docno + ", should be: " + expectedDocno + ", " + (expectedDocno == docno));
+      expectedDocno = 800; docid = "AFP_ENG_20041003.0019"; docno = dm.getDocno(docid);
+      System.out.println("dm.getDocno(" + docid + "): " + docno + ", should be: " + expectedDocno + ", " + (expectedDocno == docno));
+      System.out.println("finished testing, now exiting");
+      return 0;
+    }
+    boolean testGigaword = false;
+    if (testGigaword) {
+      for (int i = 1; i < 301; i++) {
+        docno = i * 1000;
+        docid = dm.getDocid(docno);
+        System.out.println("dm.getDocid(" + docno + "): " + docid);
+      }
+      System.out.println("finished testing, now exiting");
+      return 0;
     }
 
-	int numMappers = 100;
-	int numReducers = 100;
-	conf.setInt("Ivory.NumMapTasks", numMappers);
-	conf.setInt("Ivory.NumReduceTasks", numReducers);
+
     conf.set(Constants.CollectionName, "Aquaint2");
     conf.set(Constants.CollectionPath, collection);
     conf.set(Constants.IndexPath, indexRootPath);
@@ -155,29 +156,30 @@ public class PreprocessAquaint2 extends Configured implements Tool {
     new BuildIntDocVectorsForwardIndex(conf).run();
     new BuildTermDocVectorsForwardIndex(conf).run();
 
-	new BuildIPInvertedIndexDocSorted(conf).run();
+    new BuildIPInvertedIndexDocSorted(conf).run();
 
-	conf.set(Constants.ScoringModel, "ivory.pwsim.score.TfIdf");
-	conf.setBoolean(Constants.Normalize, true);
+    conf.set(Constants.ScoringModel, "ivory.pwsim.score.TfIdf");
+    conf.setBoolean(Constants.Normalize, true);
 
-	new BuildIntPostingsForwardIndex(conf).run();
+    new BuildIntPostingsForwardIndex(conf).run();
 
 
-	boolean buildingVectors = true;
-	if (buildingVectors) {
-		new BuildWeightedIntDocVectors(conf).run();
+    //boolean buildingVectors = true;
+    boolean buildingVectors = false;
+    if (buildingVectors) {
+      new BuildWeightedIntDocVectors(conf).run();
 
-		conf.setBoolean(Constants.BuildWeighted, true);
-		new BuildIntDocVectorsForwardIndex(conf).run();
+      conf.setBoolean(Constants.BuildWeighted, true);
+      new BuildIntDocVectorsForwardIndex(conf).run();
 
-		String findexDirPath = indexRootPath + "/findex";
-		String findexFilePath = indexRootPath + "/findex.dat";
-		if (fs.exists(new Path(findexDirPath))) {
-			LOG.info("ForwardIndex already exists: Skipping!");
-		} else {
-			new BuildAquaint2ForwardIndex ().runTool (conf, collection, findexDirPath, findexFilePath, mappingFile.toString ());
-		}
-	}
+      String findexDirPath = indexRootPath + "/findex";
+      String findexFilePath = indexRootPath + "/findex.dat";
+      if (fs.exists(new Path(findexDirPath))) {
+        LOG.info("ForwardIndex already exists: Skipping!");
+      } else {
+        new BuildAquaint2ForwardIndex ().runTool (conf, collection, findexDirPath, findexFilePath, mappingFile.toString ());
+      }
+    }
 
     return 0;
   }
