5f447ffe826c3916550fd5f541589e5d2d5fb92e
Ferhan Ture <fture@cs.umd.edu>
2013-11-19 23:40:10 -0500

    Adjusted AP values for En-Zh CLIR with Moses, following Java upgrade on cluster. Also some cosmetic changes to other regression tests.

--- src/java/regression/ivory/regression/sigir2013/moses/EnAr_TREC02.java
+++ src/java/regression/ivory/regression/sigir2013/moses/EnAr_TREC02.java
@@ -176,21 +176,16 @@ public class EnAr_TREC02 {
   }
 
   public static void main(String[] args) {
+    EnAr_TREC02.initialize();
     //    HMapSFW gridAPMap = array2Map(Interp_AP);
     HMapSFW tenbestAPMap = array2Map(Nbest_AP.get(2));
     HMapSFW onebestAPMap = array2Map(Onebest_AP.get(1));
     HMapSFW grammarAPMap = array2Map(grammar_AP.get(0));
     HMapSFW tokenAPMap = array2Map(baseline_token_AP);
-    //    System.out.println(countNumberOfImprovedTopics(tokenAPMap, gridAPMap));
-    System.out.println(countNumberOfImprovedTopics(tokenAPMap, tenbestAPMap));
-    System.out.println(countNumberOfImprovedTopics(tokenAPMap, onebestAPMap));
-    System.out.println(countNumberOfImprovedTopics(tokenAPMap, grammarAPMap));
-    System.out.println(countNumberOfImprovedTopics(tokenAPMap, tokenAPMap));
-    //    System.out.println(countNumberOfNegligibleTopics(tokenAPMap, gridAPMap));
-    System.out.println(countNumberOfNegligibleTopics(tokenAPMap, tenbestAPMap));
-    System.out.println(countNumberOfNegligibleTopics(tokenAPMap, onebestAPMap));
-    System.out.println(countNumberOfNegligibleTopics(tokenAPMap, grammarAPMap));
-    System.out.println(countNumberOfNegligibleTopics(tokenAPMap, tokenAPMap));
+
+    System.out.println("10best: improved=" + countNumberOfImprovedTopics(tokenAPMap, tenbestAPMap) + ", negligible=" + countNumberOfNegligibleTopics(tokenAPMap, tenbestAPMap));
+    System.out.println("Grammar: improved=" + countNumberOfImprovedTopics(tokenAPMap, grammarAPMap) + ", negligible=" + countNumberOfNegligibleTopics(tokenAPMap, grammarAPMap));
+    System.out.println("1best: improved=" + countNumberOfImprovedTopics(tokenAPMap, onebestAPMap) + ", negligible=" + countNumberOfNegligibleTopics(tokenAPMap, onebestAPMap));
   }
 
   private static int countNumberOfImprovedTopics(HMapSFW tokenAPMap, HMapSFW gridAPMap) {
