18a95a4aba6259fa6aa8336919742c98dd89021f
lintool <jimmylin@umd.edu>
2014-07-24 20:27:30 -0400

    fixed NPE, added ability to read multiple query files.

--- src/java/main/ivory/core/util/ConvertQueriesToIds.java
+++ src/java/main/ivory/core/util/ConvertQueriesToIds.java
@@ -2,19 +2,11 @@ package ivory.core.util;
 
 import ivory.core.ConfigurationException;
 import ivory.core.RetrievalEnvironment;
-import ivory.core.data.index.Posting;
-import ivory.core.data.index.PostingsList;
-import ivory.core.data.index.PostingsListDocSortedPositional.PostingsReader;
 
-import java.io.IOException;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -23,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.Maps;
@@ -33,49 +24,43 @@ public class ConvertQueriesToIds {
     // Turn off all logging
     ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)).setLevel(ch.qos.logback.classic.Level.OFF);
 
-    if (args.length != 2 ) {
-      System.err.println("usage: [indexPath] [queries]");
+    if (args.length < 2 ) {
+      System.err.println("usage: [indexPath] [queries]...");
     }
 
-    String qFile = args[0];
-    String indexPath = args[1];
+    String indexPath = args[0];
 
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.getLocal(conf);
 
-    Document d = null;
-
-    try {
-      d = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-          .parse(fs.open(new Path(qFile)));
-    } catch (SAXException e) {
-      throw new ConfigurationException(e.getMessage());
-    } catch (IOException e) {
-      throw new ConfigurationException(e.getMessage());
-    } catch (ParserConfigurationException e) {
-      throw new ConfigurationException(e.getMessage());
-    }
-
     Map<String, String> queries = Maps.newLinkedHashMap();
-    NodeList queryNodes = d.getElementsByTagName("query");
 
-    for (int i = 0; i < queryNodes.getLength(); i++) {
-      // Get query XML node.
-      Node node = queryNodes.item(i);
+    for (int n = 1; n < args.length; n++) {
+      String qFile = args[n];
 
-      // Get query id.
-      String qid = XMLTools.getAttributeValueOrThrowException(node, "id",
-          "Must specify a query id attribute for every query!");
+      Document d = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+          .parse(fs.open(new Path(qFile)));
 
-      // Get query text.
-      String queryText = node.getTextContent();
+      NodeList queryNodes = d.getElementsByTagName("query");
 
-      // Add query to internal map.
-      if (queries.get(qid) != null) {
-        throw new ConfigurationException(
-            "Duplicate query ids not allowed! Already parsed query with id=" + qid);
+      for (int i = 0; i < queryNodes.getLength(); i++) {
+        // Get query XML node.
+        Node node = queryNodes.item(i);
+
+        // Get query id.
+        String qid = XMLTools.getAttributeValueOrThrowException(node, "id",
+            "Must specify a query id attribute for every query!");
+
+        // Get query text.
+        String queryText = node.getTextContent();
+
+        // Add query to internal map.
+        if (queries.get(qid) != null) {
+          throw new ConfigurationException(
+              "Duplicate query ids not allowed! Already parsed query with id=" + qid);
+        }
+        queries.put(qid, queryText);
       }
-      queries.put(qid, queryText);
     }
 
     RetrievalEnvironment env = new RetrievalEnvironment(indexPath, fs);
--- src/java/main/ivory/core/util/ExtractPostingsForQueries.java
+++ src/java/main/ivory/core/util/ExtractPostingsForQueries.java
@@ -6,14 +6,13 @@ import ivory.core.data.index.Posting;
 import ivory.core.data.index.PostingsList;
 import ivory.core.data.index.PostingsListDocSortedPositional.PostingsReader;
 
-import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
@@ -22,8 +21,9 @@ import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class ExtractPostingsForQueries {
@@ -31,55 +31,50 @@ public class ExtractPostingsForQueries {
     // Turn off all logging
     ((ch.qos.logback.classic.Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)).setLevel(ch.qos.logback.classic.Level.OFF);
 
-    if (args.length != 2 ) {
-      System.err.println("usage: [indexPath] [queries]");
+    if (args.length < 2 ) {
+      System.err.println("usage: [indexPath] [queries]...");
     }
 
-    String qFile = args[0];
-    String indexPath = args[1];
+    String indexPath = args[0];
 
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.getLocal(conf);
 
-    Document d = null;
-
-    try {
-      d = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-          .parse(fs.open(new Path(qFile)));
-    } catch (SAXException e) {
-      throw new ConfigurationException(e.getMessage());
-    } catch (IOException e) {
-      throw new ConfigurationException(e.getMessage());
-    } catch (ParserConfigurationException e) {
-      throw new ConfigurationException(e.getMessage());
-    }
-
     Map<String, String> queries = Maps.newLinkedHashMap();
-    NodeList queryNodes = d.getElementsByTagName("query");
+    List<String> files = Lists.newArrayList();
+    for (int n = 1; n < args.length; n++) {
+      String qFile = args[n];
+      files.add(qFile);
+      Document d = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+          .parse(fs.open(new Path(qFile)));
 
-    for (int i = 0; i < queryNodes.getLength(); i++) {
-      // Get query XML node.
-      Node node = queryNodes.item(i);
+      NodeList queryNodes = d.getElementsByTagName("query");
 
-      // Get query id.
-      String qid = XMLTools.getAttributeValueOrThrowException(node, "id",
-          "Must specify a query id attribute for every query!");
+      for (int i = 0; i < queryNodes.getLength(); i++) {
+        // Get query XML node.
+        Node node = queryNodes.item(i);
 
-      // Get query text.
-      String queryText = node.getTextContent();
+        // Get query id.
+        String qid = XMLTools.getAttributeValueOrThrowException(node, "id",
+            "Must specify a query id attribute for every query!");
 
-      // Add query to internal map.
-      if (queries.get(qid) != null) {
-        throw new ConfigurationException(
-            "Duplicate query ids not allowed! Already parsed query with id=" + qid);
+        // Get query text.
+        String queryText = node.getTextContent();
+
+        // Add query to internal map.
+        if (queries.get(qid) != null) {
+          throw new ConfigurationException(
+              "Duplicate query ids not allowed! Already parsed query with id=" + qid);
+        }
+        queries.put(qid, queryText);
       }
-      queries.put(qid, queryText);
     }
 
     RetrievalEnvironment env = new RetrievalEnvironment(indexPath, fs);
     env.initialize(true);
 
-    System.out.println("# Postings dump of " + indexPath + " for queries " + qFile + ": " + new Date());
+    System.out.println("# Postings dump of " + indexPath + " for queries " +
+        Joiner.on(",").join(files) + ": " + new Date());
     System.out.println("# docs: " + env.getDocumentCount());
     System.out.println("# collection size: " + env.getCollectionSize());
     System.out.println("# vocab size: " + env.getDictionary().size());
@@ -96,11 +91,15 @@ public class ExtractPostingsForQueries {
 
     for (Map.Entry<String, Integer> e : terms.entrySet()) {
       PostingsList postings = env.getPostingsList(e.getKey());
+      if (postings == null) {
+        continue;
+      }
+
       PostingsReader r = (PostingsReader) postings.getPostingsReader();
       Posting p = new Posting();
       System.out.print(e.getValue() + "=");
       while (r.nextPosting(p)) {
-        System.out.print(p.getDocno() + ":" + p.getTf() + ",");
+        System.out.print(p.getDocno() + ":" + p.getTf() + ", ");
       }
       System.out.println("");
     }
