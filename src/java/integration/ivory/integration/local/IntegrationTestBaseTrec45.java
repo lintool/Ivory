17fb08d5e46ef1ea37133391e9591b77784eb77d
lintool <jimmylin@umd.edu>
2014-12-10 20:14:44 -0500

    Fixed integration tests in ivory.integration.local.

--- src/java/integration/ivory/integration/local/IntegrationTestBaseTrec45.java
+++ src/java/integration/ivory/integration/local/IntegrationTestBaseTrec45.java
@@ -39,6 +39,7 @@ public class IntegrationTestBaseTrec45 {
     jars.add(IntegrationUtils.getJar("lib", "sux4j"));
     jars.add(IntegrationUtils.getJar("lib", "commons-collections"));
     jars.add(IntegrationUtils.getJar("lib", "kamikaze"));
+    jars.add(IntegrationUtils.getJar("lib", "lintools-datatypes-1.0.0"));
     jars.add(IntegrationUtils.getJar("dist", "ivory"));
 
     String libjars = String.format("-libjars=%s", Joiner.on(",").join(jars));
